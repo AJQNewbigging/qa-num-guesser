0f39187e54abf8c60e5b1036f3bb2f0d284d8d15
Asher Newbigging <asher.newbigging@outlook.com>
2022-03-21 16:11:30 +0000

    AN - Add lives

--- src/com/qa/guesser/Game.java
+++ src/com/qa/guesser/Game.java
@@ -7,18 +7,27 @@ public class Game {
 	public static void guesser() {
 		RandomNumber randNo = new RandomNumber();
 		int ourNumber = randNo.getRandomNumber();
+		int lives = 7;
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Try to guess the number");
-		int userInput = sc.nextInt();
-		System.out.println(userInput);
+		System.out.println("Try to guess the number:");
+		int userInput = 0;
+		boolean hasGuessed = false;
 		
-		while (userInput != ourNumber) {
-			System.out.println("Wrong number guess again");
+		while (!hasGuessed && lives > 0) {
 			userInput = sc.nextInt();
+			
+			hasGuessed = userInput == ourNumber;
+			if (!hasGuessed && --lives > 0) {
+				System.out.printf("Wrong number! You have %d lives remaining\nGuess again:\n", lives);
+			} 
 		}
 
-		
+		if (hasGuessed) {
+			System.out.println("You guessed correctly!");
+		} else {
+			System.out.println("Ran out of lives! Better luck next time.");
+		}
 		
 		
 	}
