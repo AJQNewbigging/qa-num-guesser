57f9d9f29ebcc3ccf97fb165fb511279802c5b1d
Mohab <khalifamohab97@outlook.com>
2022-03-21 15:20:21 +0000

    MK: created Game class

---
+++ src/com/qa/guesser/Game.java
@@ -0,0 +1,14 @@
+package com.qa.guesser;
+
+import java.util.Scanner;
+
+public class Game {
+
+	public static void guesser() {
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+		System.out.println(userInput);
+
+	}
+
+}
